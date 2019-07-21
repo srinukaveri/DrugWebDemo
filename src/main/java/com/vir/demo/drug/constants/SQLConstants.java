6525c3c19b9a0aecad4fe5c044dda45f6ad12340
Srinu <sreenivasulu.kaveripakam@ibm.com>
2019-07-21 17:10:21 +0530

    SQL query condition update

--- src/main/java/com/vir/demo/drug/constants/SQLConstants.java
+++ src/main/java/com/vir/demo/drug/constants/SQLConstants.java
@@ -7,7 +7,7 @@ package com.vir.demo.drug.constants;
 public class SQLConstants {
 
 	public static final String LOGIN_SQL = "from UserLoginDetails p  where p.userName =: userName";
-	public static final String GET_PHARMACY_SQL = " from PharmacyDetails p where p.area  =: area";
+	public static final String GET_PHARMACY_SQL = " from PharmacyDetails p where p.isRegistered = 'Y' and p.area  =: area";
 	public static final String GET_DRUG_SQL = " from DrugDetails d where d.isActive = 'Y' order by d.drugName asc";
 	public static final String USER_NAME = "userName";
 	public static final String DRUG_NAME = "drugName";
@@ -19,7 +19,7 @@ public class SQLConstants {
 			+  "pd.pharmacyMasterId, d.drugId,d.drugName,d.isActive, "
 			+ " pd.mappingId,pd.isAvailable,pd.drugPriceEach,pd.currency) "
 			+" from DrugDetails d inner join PharmacyDrugMaster pd on d.drugId = pd.drugId "
-			+ " where  pd.pharmacyMasterId in (:pharmacyMasterId) and d.drugName in (:drugName)";
+			+ " where d.isActive = 'Y' and pd.isAvailable = 'Y' and pd.pharmacyMasterId in (:pharmacyMasterId) and d.drugName in (:drugName)";
 	
 	
 	public static final String LATEST_DRUG_ID = "select new com.vir.demo.demo.drug.model.DrugSearch(max(d.drugId)) from "
