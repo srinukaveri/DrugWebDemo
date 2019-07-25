3af2e44107baad8661f21bf5f0ae719348c979c1
Srinu <sreenivasulu.kaveripakam@ibm.com>
2019-07-25 08:17:29 -0600

    pharmacy list service

--- src/main/java/com/vir/demo/drug/constants/SQLConstants.java
+++ src/main/java/com/vir/demo/drug/constants/SQLConstants.java
@@ -10,7 +10,7 @@ public class SQLConstants {
 	public static final String GET_PHARMACY_SQL = " from PharmacyDetails p where p.isRegistered = 'Y' and p.area  =: area";
 	public static final String GET_ALL_PHARMACY_SQL = " from PharmacyDetails p where p.isRegistered = 'Y' order by p.area asc";
 	public static final String GET_PHARMACY_AREA_SQL = "select distinct(p.area) from PharmacyDetails p where p.isRegistered = 'Y' order by p.area asc";
-	public static final String GET_PHARMACY_LIST_SQL = "select distinct(p.pharmacyName) from PharmacyDetails p order by p.area asc";
+	public static final String GET_PHARMACY_LIST_SQL = "select distinct(p.pharmacyName) from PharmacyDetails p order by p.pharmacyName asc";
 	public static final String GET_DRUG_SQL = " from DrugDetails d where d.isActive = 'Y' order by d.drugName asc";
 	public static final String USER_NAME = "userName";
 	public static final String DRUG_NAME = "drugName";
